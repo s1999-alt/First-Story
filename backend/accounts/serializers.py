8f2c765f21ac3053504aae66c23c1d87fcad2b2b
Siyad Savad <siyadsavad313@gmail.com>
2026-01-29 16:41:15 +0530

    created the Product app for the products, add it in the settings.py app, add its in the main urls

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Item
+from .models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('accounts.urls')),
-    # path('api/products/', include('books.urls')),
+    path('api/Products/', include('Products.urls')),
 ]
 
 if settings.DEBUG:
